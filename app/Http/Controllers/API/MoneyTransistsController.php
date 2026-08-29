d9b4f979c0d3b0cf4f3b1bb781796aa1a2c1b100
HasalKenula <hasalkenula255@gmail.com>
2026-08-29 19:54:18 +0530

    fix a error in moneyTransists Controller

--- app/Http/Controllers/API/MoneyTransistsController.php
+++ app/Http/Controllers/API/MoneyTransistsController.php
@@ -35,7 +35,7 @@ class MoneyTransistsController extends Controller
 
         $head = $this->padNumber($estimate->head, 4);
         $project = $this->padNumber($estimate->project, 2);
-        $object = $this->padNumber($estimate->object, 3);
+        $object = $this->padNumber($estimate->object, 2);
         
         return "{$head}-{$project}-{$object}";
     }
